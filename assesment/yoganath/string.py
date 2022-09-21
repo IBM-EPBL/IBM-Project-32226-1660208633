a08c47bed72d255f31dd4fd5c75093395b3df857
whynotvikash <113960683+whynotvikash@users.noreply.github.com>
2022-09-21 20:41:56 +0530

    Add files via upload

---
+++ assesment/yoganath/string.py
@@ -0,0 +1,12 @@
+n1="vikash"
+n2="yoganath"
+print("concatenated string:")
+print((n1+n2))
+
+str='vikash'
+print("Reversed string:")
+print((str[::-1]))
+
+str='yoganath'
+print("Sliced string:")
+print((str[2:14]))
